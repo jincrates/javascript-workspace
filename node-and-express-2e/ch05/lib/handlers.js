da0c89c075c29c6dd626e2c1b8f77094c6533856
jincrates <94jingyu@gmail.com>
2022-10-08 09:52:19 +0900

    eslint 적용

--- node-and-express-2e/ch05/lib/handlers.js
+++ node-and-express-2e/ch05/lib/handlers.js
@@ -2,8 +2,14 @@ const fortune = require('./fortune')
 
 exports.home = (req, res) => res.render('home')
 
-exports.about = (req, res) => res.render('about', { fortune: fortune.getFortune() })
+exports.about = (req, res) =>
+  res.render('about', { fortune: fortune.getFortune() })
 
 exports.notFound = (req, res) => res.render('404')
 
+// 익스프레스는 매개변수가 네 개 있어야 오류 핸들러를 인식하므로
+// next 매개변수는 사용하지 않더라도 그냥 둬야 합니다.
+// 따라서 다음 행에 한해 ES린트의 no-unused-vars 규칙을 비활성화합니다.
+/* eslint-disable-next-line no-unused-vars */
 exports.serverError = (err, req, res, next) => res.render('500')
+/* eslint-disable-next-line no-unused-vars */
--- node-and-express-2e/ch05/meadowlark.js
+++ node-and-express-2e/ch05/meadowlark.js
@@ -26,6 +26,7 @@ app.use(handlers.notFound)
 // custom 500 page
 app.use(handlers.serverError)
 
+// require.main이 전역 module과 일치하면 노드에서 자바스크립트 파일을 직접 실행하고, 그렇지 않다면 다른 스크립트로 임포트한다.
 if(require.main === module) {
     app.listen(port, () => console.log(
         `Express started on http://localhost:${port}; ` + 
