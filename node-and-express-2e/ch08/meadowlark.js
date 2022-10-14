76f5201f2b006f3b14378269a1378917d216dd43
jincrates <94jingyu@gmail.com>
2022-10-14 09:02:29 +0900

    플래시 메시징

--- node-and-express-2e/ch08/meadowlark.js
+++ node-and-express-2e/ch08/meadowlark.js
@@ -48,6 +48,7 @@ app.get('/', handlers.home)
 app.get('/newsletter-signup', handlers.newsletterSignup)
 app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+app.get('/newsletter-archive', handlers.newsletterArchive)
 
 // handlers for fetch/JSON form submission
 app.get('/newsletter', handlers.newsletter)
